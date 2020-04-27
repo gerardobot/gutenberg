4ac2be1ef3ec9cf769d6eb3dcb77492ebd7e5932
Andrew Duthie <andrew@andrewduthie.com>
2020-04-27 10:08:27 -0400

    Block API: WP_Block: Pass block as third argument of render_callback

--- packages/block-library/src/post-title/index.php
+++ packages/block-library/src/post-title/index.php
@@ -10,13 +10,12 @@
  *
  * @return string Returns the filtered post title for the current post wrapped inside "h1" tags.
  */
-function render_block_core_post_title() {
-	global $_experimental_block;
-	if ( ! isset( $_experimental_block->context['postId'] ) ) {
+function render_block_core_post_title( $attributes, $content, $block ) {
+	if ( ! isset( $block->context['postId'] ) ) {
 		return '';
 	}
 
-	return '<h1>' . get_the_title( $_experimental_block->context['postId'] ) . '</h1>';
+	return '<h1>' . get_the_title( $block->context['postId'] ) . '</h1>';
 }
 
 /**
